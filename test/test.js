50849784dbaeb6516a1b595644ffac2d2ccee051
Isaac Simmons <ids23@drexel.edu>
2015-09-15 00:01:09 -0400

    Initial outline for tests

---
+++ test/test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var config
+
+var middleware = require('../index.js');
+
+var lastTimestamp = null;
+
+function addFile(path) {
+
+}
+
+function touchFile(path) {
+
+}
+
+function delFile(path) {
+
+}
+
+function addDir(path) {
+
+}
+
+function rmDir(path) {
+
+}
+
+function fakeReq(server, callback) {
+  var headers = [];
+  var buf = [];
+
+  server(null, {
+    end: function(s) {
+      if (typeof s === 'string') {
+        buf.push(s);
+      }
+      callback(headers, buf.join(''));
+    },
+    write: function(s) {
+      if (typeof s === 'string') {
+        buf.push(s);
+      }
+    },
+    set: function(name, value) {
+      headers.push({name: name, value: value});
+    }
+  });
+}
+
+
+function assertInManifest(server, line) {
+
+}
+
+function getManifestComment(server) {
+  console.log('get comment!');
+  fakeReq(server, function(headers, body) {
+    console.log(body.split('\n'));
+  });
+}
+
+
+
+function assertManifestChanged(server) {
+
+}
+
+function assertManifestNotChanged(server) {
+
+}
+
+
+middleware.generator('test', function(server) {
+  getManifestComment(server);
+  server.stop();
+});
+
