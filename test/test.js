afde101dab28c05e4ede774cb45249ac9d8d2bef
Isaac Simmons <ids23@drexel.edu>
2015-09-15 14:49:27 -0400

    Add a testing directory and some test files. Check for their presence

--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var assert = require("assert");
+var fs = require('fs');
 var middleware = require('../index.js');
-
-var CONFIG = {
-  file: 'test'
-};
 
 //Helper Functions
 function fakeReq(server, callback) {
@@ -27,6 +24,15 @@ function fakeReq(server, callback) {
     set: function(name, value) {
       headers.push({name: name, value: value});
     }
+  });
+}
+
+function getManifest(server, callback) {
+  fakeReq(server, function(header, body) {
+    var manifest = {};
+    var lines = body.split('\n');
+
+    callback(lines); //TODO: return manifest instead
   });
 }
 
@@ -54,14 +60,21 @@ function assertInManifest(server, line) {
 
 }
 
-function getManifestComment(server) {
+function getManifestComment(server, callback) {
   console.log('get comment!');
   fakeReq(server, function(headers, body) {
-    console.log(body.split('\n'));
+    body = body.split('\n');
+    var commentLine = null;
+    for(var i = 0; i < body.length; i--) {
+      if (body[i].startsWith('#')) {
+        assert.equal(commentLine, null, 'Multiple comment lines found in manifest');
+        commentLine = body[i];
+      }
+    }
+    assert.notEqual(commentLine, null, 'No comment found in manifest');
+    callback(commentLine);
   });
 }
-
-
 
 function assertManifestChanged(server) {
 
@@ -71,13 +84,61 @@ function assertManifestNotChanged(server) {
 
 }
 
+var CONFIG = [
+  {
+    file: 'test_files/some_files',
+    url: 'some'
+  },
+  'test_files/more_files'
+];
+
+var INITIAL_FILES = [
+  'test_files/some_files/a.txt',
+  'test_files/some_files/b.txt',
+  'test_files/some_files/nested/x.txt',
+  'test_files/some_files/nested/y.txt',
+  'test_files/more_files/1.txt',
+  'test_files/more_files/2.txt'
+];
+
+var INITIAL_URLS = [
+  '/some/a.txt',
+  '/some/b.txt',
+  '/some/nested/x.txt',
+  '/some/nested/y.txt',
+  '/test_files/more_files/1.txt',
+  '/test_files/more_files/2.txt'
+];
+
 //Tests
-describe('Initial Scan', function() {
+describe('Init', function() {
   it('should initialize properly', function (done) {
-    middleware.generator(CONFIG, function(server) {
-      //getManifestComment(server);
+    middleware.generator(CONFIG, null, function(server) {
       server.stop();
       done();
+    });
+  });
+});
+
+describe('Check filesystem', function() {
+  it('Should contain expected files', function() {
+    for(var i = 0; i < INITIAL_FILES.length; i++) {
+      var path = INITIAL_FILES[i];
+      fs.stat(path, function(err, stat) {
+        assert.equal(err, null, 'Error getting fs stat for ' + path);
+        assert(stat.isFile(), 'Missing initial file ' + path);
+      });
+    }
+  });
+});
+
+describe('Check initial data', function() {
+  it('should contain expected elements', function (done) {
+    middleware.generator(CONFIG, null, function(server) {
+      getManifest(server, function(header, body){
+        server.stop();
+        done();
+      });
     });
   });
 });
